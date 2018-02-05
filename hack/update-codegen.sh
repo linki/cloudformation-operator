a16c3a1e6cc1f17fa3e7e7136017a6ea7f2a6840
Martin Linkhorst <martin.linkhorst@zalando.de>
2018-02-05 11:46:38 +0100

    chore: fix and clean up dependencies

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -7,7 +7,7 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ${GOPATH}/src/k8s.io/code-generator)}
 
-vendor/k8s.io/code-generator/generate-groups.sh all \
+${SCRIPT_ROOT}/hack/generate-groups.sh all \
   github.com/linki/cloudformation-operator/pkg/client github.com/linki/cloudformation-operator/pkg/apis \
   cloudformation:v1alpha1 \
   --go-header-file ${SCRIPT_ROOT}/hack/no-boilerplate.go.txt
